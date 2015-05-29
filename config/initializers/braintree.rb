43dc258ffbf99e858d61e27195734bad54bc47de
divshah <divshah@paypal.com>
2015-05-29 13:10:08 -0700

    Braintree configuration changes

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,5 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "9yrtqbh6v3d7929g"
-Braintree::Configuration.public_key = "w7cbprq7s5kzjb45"
-Braintree::Configuration.private_key = "44f692cd30d80aa8557bf648cfbb07a0"
+Braintree::Configuration.merchant_id = "<merchant_id>"
+Braintree::Configuration.public_key = "<public_kye>"
+Braintree::Configuration.private_key = "<private_key>"
+
